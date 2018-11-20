f1cde6d25d3576c0afd8bf6096ba45fb682abc86
Fei Qi <feiqi9047@gmail.com>
2018-11-20 05:43:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,20 +12,15 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-  let index = 0;
-  document.body.addEventListener('keydown',function(e) {
-  const key = e.key;
-
-    if (key === codes[index]) {
-      index++;
-
-      if (index === codes.length) {
-        alert("Congratulations");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  })}
+let i = 0
+document.body.addEventListener('keyDown', 
+function(e){
+  const key = e.key
+  if (key===codes[i]){i++;
+if (i===codes.length){
+  alert('Congrats!');
+  i = 0;
+}
+}else{
+  i=0;
+}})}
